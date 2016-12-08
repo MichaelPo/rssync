63202d744bbe07979bea219586dcb37c4825d456
Ponewass <michael.ponewass@lvdigital.de>
2016-12-08 17:39:13 +0100

    svc

---
+++ database/migrations/2016_12_08_162846_create_favorites_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFavoritesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('favorites', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_fk_id')->unsigned();
+            $table->foreign('user_fk_id')->references('id')->on('users');
+            $table->string('title');
+            $table->string('url');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('favorites');
+    }
+}
